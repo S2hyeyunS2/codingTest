aa590883a00b93068242c88592911647cf9e77b6
김혜윤 <hhyeyun22@gmail.com>
2025-02-22 11:52:01 +0900

    [Silver III] Title: 이친수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.py
+++ 백준/Silver/2193. 이친수/이친수.py
@@ -1,12 +1,5 @@
-N=int(input())
-
-dp=[0]*(N+1)
-
-dp[1]=1
-if N>1:
-    dp[2]=1
-
-for i in range(3,N+1):
-    dp[i]=dp[i-1]+dp[i-2]
-
-print(dp[N])
+dp = [1,1]
+n = int(input())
+while(len(dp) < n):
+    dp.append(dp[-1]+dp[-2])
+print(dp[-1])

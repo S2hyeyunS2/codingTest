cec39a9d1dcbd21b639d61d6c9e93e03cb4ca252
김혜윤 <hhyeyun22@gmail.com>
2025-09-23 10:21:25 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 79.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int sum = 0;
+        
+       for(int num:numbers){
+           sum+=num;
+       }
+        
+        return 45-sum;
+    }
+}
